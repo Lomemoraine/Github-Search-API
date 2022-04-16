2fb9a8abb50648920f1b6fcd19468d21f06589fa
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-04-16 19:05:23 +0300

    import observable to enable subcription into the data and use of a promise

--- src/app/services/githubservice.service.ts
+++ src/app/services/githubservice.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import{ HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubserviceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+
+  }
 }
